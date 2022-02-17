23270c77fb68d3d140ea1e33d961df6335da9d2c
bk8p <83872174+bk8p@users.noreply.github.com>
2022-02-16 23:01:25 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Ian Vachuska.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("218731527 - This line was added by Shawn Bacani.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
