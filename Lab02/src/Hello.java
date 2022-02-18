fe61911c23c579bacdc4941f7469959dc5934b53
sawet <sawet@DESKTOP-LE45UVS>
2022-02-18 12:21:58 -0800

    Add Sirapicha

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Ian Vachuska.");
 		System.out.println("218731527 - This line was added by Shawn Bacani.");
-		System.out.println("302731390 - This line was added.");
+		System.out.println("302731390 - This line was added Sirapicha Sawettanant.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
