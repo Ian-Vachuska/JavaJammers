a7086ab22d4c3bea894eb1a4d2a5669e5dac78aa
ian <ian@localhost>
2022-02-21 14:56:51 -0800

    Added my sac id

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Ian Vachuska.");
+		System.out.println("302521752 - This line was added by Ian Vachuska.");
 		System.out.println("218731527 - This line was added by Shawn Bacani.");
 		System.out.println("302731390 - This line was added by Sirapicha Sawettanant.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
