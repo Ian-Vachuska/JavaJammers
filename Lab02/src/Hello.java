1617e97b39a8ce177e06239264b9e7af258ebe3d
Kyle Cruz <82552015+kylec-26@users.noreply.github.com>
2022-02-21 16:16:23 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("302521752 - This line was added by Ian Vachuska.");
 		System.out.println("218731527 - This line was added by Shawn Bacani.");
 		System.out.println("302731390 - This line was added by Sirapicha Sawettanant.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219775388 - This line was added by Kyle Cruz.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
